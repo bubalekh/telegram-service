8a35512596e93fde39674e290eeb54eb0a3867a6
Maksim Safronov <dev.safronov.maxim@gmail.com>
2023-01-11 00:35:39 +0300

    Small refactoring

--- src/main/java/pw/cyberbrain/telegram/service/messaging/IntegrationService.java
+++ src/main/java/pw/cyberbrain/telegram/service/messaging/IntegrationService.java
@@ -2,5 +2,5 @@ package pw.cyberbrain.telegram.service.messaging;
 
 public interface IntegrationService {
     void sendForHandling(String message);
-    void setConsumingCallback(Object producer);
+    void setConsumingCallback(Object callback);
 }
